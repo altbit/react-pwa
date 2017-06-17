bf42c25c8477c60171cab4868f546a8616b1123c
Denis Degtyarev <alt.degtyarev@gmail.com>
2017-06-17 17:46:44 +0100

    Mail service

---
+++ server/services/Mail.js
@@ -0,0 +1,26 @@
+const config = require('./../../config/config');
+const debug = require('debug')('service:mail');
+var postmark = require("postmark")(config.server.postmark.apiToken);
+
+class MailService {
+  constructor() {
+    this.sendWelcome = this.sendWelcome.bind(this);
+  }
+
+  sendWelcome(user, done) {
+    postmark.sendEmailWithTemplate({
+      "From": config.server.postmark.mailFrom,
+      "To": user.email,
+      "TemplateId": config.server.postmark.templateId,
+      "TemplateModel": {
+        "productName": config.app.appName,
+        "firstName": user.firstName,
+        "action_url": config.app.ServerApi + '/confirmEmail/' + user.verifyEmailToken,
+        "productAddressLine1": config.app.AddressLine1,
+        "productAddressLine2": config.app.AddressLine2,
+      },
+    }, done);
+  }
+}
+
+module.exports = new MailService();
